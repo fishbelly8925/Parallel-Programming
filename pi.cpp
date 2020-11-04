0572337cd914b2a3d6f3d58aa866007ec25fe58f
fishbelly8925 <aomine0205@gmail.com>
2020-11-05 00:03:58 +0800

    test2

--- pi.cpp
+++ pi.cpp
@@ -14,37 +14,33 @@
 #include <cmath>
 #include <queue>
 #include <set>
-
+#include <numeric>
 using namespace std;
 using std::string;
 using std::thread;
 
-int cnt;
 int thread_num;
 long long n;
-
-pthread_mutex_t mutex;
+long long cnt;
 
 void* pp(void* rank) {
     long local_rank = (long)rank;
-    long iter_num = (local_rank > n % thread_num) ? n / thread_num: n / thread_num + 1;
+    long iter_num = (local_rank < n % thread_num) ? n / thread_num + 1: n / thread_num;
+    unsigned int seed = 0;
 
-    double x, y, d;
+    double x, y;
     long local = 0;
-
+    
     for (long i = 0; i < iter_num; i++) {
-
-        x = (double)rand() / RAND_MAX;
-        y = (double)rand() / RAND_MAX;
-        d = x * x + y * y;
-
-        if (d < 1) local++;
+	
+        x = (double)rand_r(&seed) / RAND_MAX;
+        y = (double)rand_r(&seed) / RAND_MAX;
+        if (x * x + y * y < 1) local++;
+	
     }
+    
 
-    pthread_mutex_lock(&mutex);
-    cnt += local;
-    pthread_mutex_unlock(&mutex);
-    return NULL;
+    return (void*) local;
 }
 
 
@@ -56,23 +52,23 @@ int main(int argc, char* argv[]) {
     thread_num = stoi(argv[1], NULL, 10);
     n = stoll(argv[2], NULL, 10);
     ppthread = (pthread_t*) malloc(thread_num * sizeof(pthread_t));
-    pthread_mutex_init(&mutex, NULL);
-    cnt = 0;
+    //pthread_mutex_init(&mutex, NULL);
+    //srand(time(NULL));
+    void * returnValue;
 
-    srand(time(NULL));
-    
     for (long i = 0; i < thread_num; i++) {
-        pthread_create(&ppthread[i], NULL, pp, (void*)i);
+        pthread_create(&ppthread[i], NULL, &pp, (void*)i);
     }
 
     for (int i = 0; i < thread_num; i++) {
-        pthread_join(ppthread[i], NULL);
+        pthread_join(ppthread[i], &returnValue);
+	cnt += (long) returnValue;
     }
 
     double ans = (double) cnt / (double) n;
-    cout << setprecision(20) << ans * 4 << '\n';
+    cout << setprecision(10) << ans * 4 << '\n';
 
-    pthread_mutex_destroy(&mutex);
-    free(ppthread);
+    //pthread_mutex_destroy(&mutex);
+    
     return 0;
 }
